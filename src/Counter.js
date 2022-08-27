1b50da88f58506c232d5faf756c0c05011bdd634
Todd Pangilinan <pang.todd@gmail.com>
2022-08-27 15:15:57 -0700

    updated Counter.js to include useEffect.

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Counter(){
   const [counter, setCounter] = useState(0);
   const [hidden, setHidden] = useState(true)
 
+  useEffect(()=> {
+    console.log("EFFECT!");
+  });
+
   return (
     <React.Fragment>
       {hidden ? <h1>{counter}</h1>: <h1>Count Hidden</h1>}
-      <button onClick={()=> setCounter(counter +1)}>COUNT!</button>
+      <button onClick={()=> setCounter(counter +1)}>COUNT!</button> 
+      <br></br>
+      <br></br>
       <button onClick={()=> setHidden(!hidden)}>Hide/Seek</button>
 
     </React.Fragment>
