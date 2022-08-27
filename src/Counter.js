4a37f8aca999ef5e198219ccef24d92221ffaa2d
Todd Pangilinan <pang.todd@gmail.com>
2022-08-27 14:50:15 -0700

    added button, app works

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 
 function Counter(){
+  const [counter, setCounter] = useState(0);
+
   return (
     <React.Fragment>
+      <h1>{counter}</h1>
+      <button onClick={()=> setCounter(counter +1)}>COUNT!</button>
     </React.Fragment>
   )
 }
