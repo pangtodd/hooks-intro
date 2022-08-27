ae6ceaafbba7a48047bbe00d94d9b0b60446d60b
Todd Pangilinan <pang.todd@gmail.com>
2022-08-27 14:54:17 -0700

    added 'hide/show' button and logic.

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 function Counter(){
   const [counter, setCounter] = useState(0);
+  const [hidden, setHidden] = useState(true)
 
   return (
     <React.Fragment>
-      <h1>{counter}</h1>
+      {hidden ? <h1>{counter}</h1>: <h1>Count Hidden</h1>}
       <button onClick={()=> setCounter(counter +1)}>COUNT!</button>
+      <button onClick={()=> setHidden(!hidden)}>Hide/Seek</button>
+
     </React.Fragment>
   )
 }
