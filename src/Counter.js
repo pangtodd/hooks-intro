265a891a00f41c6e195663d6601be38a4e0efc8c
Todd Pangilinan <pang.todd@gmail.com>
2022-09-11 11:42:47 -0700

    updated app to work with reducer for counter.

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
+
+const bobby={
+  counter: 0
+}
+
+function brown(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {
+        counter: state.counter +1
+      };
+      default:
+        throw new Error(`There is no action matching ${action.type}.`);
+
+  }
+}
 
 function Counter(){
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(brown, bobby);
   const [hidden, setHidden] = useState(true)
 
   useEffect(()=> {
-    console.log("EFFECT!");
-    document.title = counter;
-  }, [counter] );
+    document.title = state.counter;
+  }, [state.counter] );
 
   return (
     <React.Fragment>
-      {hidden ? <h1>{counter}</h1>: <h1>Count Hidden</h1>}
-      <button onClick={()=> setCounter(counter +1)}>COUNT!</button> 
+      {hidden ? <h1>{state.counter}</h1>: <h1>Count Hidden</h1>}
+      <button onClick={()=> dispatch({type: 'increment'})}>COUNT!</button> 
       <br></br>
       <br></br>
       <button onClick={()=> setHidden(!hidden)}>Hide/Seek</button>
-
     </React.Fragment>
   )
 }
